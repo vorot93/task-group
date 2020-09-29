2e473ed6c0fa118cc76c8638761dcb8d29ce75b8
Artem Vorotnikov <artem@vorotnikov.me>
2020-09-29 17:57:49 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use futures::future::abortable;
+use parking_lot::Mutex;
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+#[derive(Clone, Debug)]
+pub enum Shutdown {
+    Runtime,
+    TaskGroup,
+}
+
+pub struct TaskHandle<T>(Pin<Box<dyn Future<Output = Result<T, Shutdown>> + Send + 'static>>);
+
+impl<T> Future for TaskHandle<T>
+where
+    T: Send + 'static,
+{
+    type Output = Result<T, Shutdown>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Pin::new(&mut self.0).poll(cx)
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct TaskGroup(Mutex<Vec<futures::future::AbortHandle>>);
+
+impl TaskGroup {
+    pub fn spawn<Fut, T>(&self, future: Fut) -> TaskHandle<T>
+    where
+        Fut: Future<Output = T> + Send + 'static,
+        T: Send + 'static,
+    {
+        let mut group = self.0.lock();
+        let (t, handle) = abortable(future);
+        group.push(handle);
+        let spawned_handle = tokio::spawn(t);
+        TaskHandle(Box::pin(async move {
+            Ok(spawned_handle
+                .await
+                .map_err(|_| Shutdown::Runtime)?
+                .map_err(|_| Shutdown::TaskGroup)?)
+        }))
+    }
+}
+
+impl Drop for TaskGroup {
+    fn drop(&mut self) {
+        for handle in &*self.0.lock() {
+            handle.abort();
+        }
+    }
+}
